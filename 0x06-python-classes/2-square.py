b15b6c5c334b689b6f2560f10381877a022af0f8
freedom469 <bamjnr99@gmail.coom>
2022-12-22 02:13:12 +0000

    stash

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,21 +1,43 @@
 #!/usr/bin/python3
 
+"""A module that defines a square """
+
+
+
+
 
 class Square:
-    """creats a square """
+
+    """A class that represents a square"""
+
+
 
     def __init__(self, size=0):
-        """initializes a square
-	Args:
-            size: size of the square
-	Raises:
-        TypeError: if size is not an integer
-	ValueError: if size is less than zero
-	"""
-        if isinstance(size, int) == False:
-            raise TypeError("size must be an integer")
+
+        """Initializing this square class
+
+        Args:
+
+            size: represnets the size of the square defined
+
+        Raises:
+
+            TypeError: if size is not integer
+
+            ValueError: if size is less than zero
+
+        """
+
+
+
+        if not isinstance(size, int):
+
+            raise TypeError('size must be an integer')
+
         if size < 0:
+
             raise ValueError('size must be >= 0')
 
-        self.__size = size
 
+
+        self.__size = sizei
