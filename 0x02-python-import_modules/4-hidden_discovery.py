7f70a6032cc1433085e780df06eb7dceb32a1d3e
Malik <bamjnr99@gmail.com>
2023-01-12 18:03:04 -0800

    fixed conflit

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-if __name__ == "__main__":
-    import hidden_4
-    for i in dir(hidden_4):
-        if i.startswith("_"):
-=======
 
 if __name__ == "__main__":
     import hidden_4
 
     for i in dir(hidden_4):
         if i.startswith("__"):
->>>>>>> 5cd30d0f10ed0ee1021d0e5d3fc2351d69baef75
             continue
         print(i)
