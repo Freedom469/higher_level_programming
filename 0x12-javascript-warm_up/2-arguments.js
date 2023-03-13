4a0b8ccc6c07f0c150a86af13b5188cf50762541
Malik <bamjnr99@gmail.com>
2023-03-13 08:57:51 -0700

    fixed output

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,6 +2,13 @@
 
 if (process.argv.length === 2) {
   console.log('No argument');
-} else {
-  console.log('Argument found');
+}
+else if (process.argv.length === 3) {
+
+	console.log('Argument found');
+}
+
+
+else {
+  console.log('Arguments found');
 }
