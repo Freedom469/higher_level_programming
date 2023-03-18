71b6377e03a363e932f72a371c8d0bdad91d7391
Malik <bamjnr99@gmail.com>
2023-03-18 05:57:55 -0700

    a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa:

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,13 +11,16 @@ if __name__ == "__main__":
     try:
         connect = MySQLdb.connect(
                     host="localhost",
+                    port=3306,
                     user=argv[1],
                     password=argv[2],
-                    database=argv[3]
+                    database=argv[3],
+                    charset='utf8'
 
                     )
 
     except MySQLdb.Error:
+
         print("Connection failed")
 
     cursor = connect.cursor()
