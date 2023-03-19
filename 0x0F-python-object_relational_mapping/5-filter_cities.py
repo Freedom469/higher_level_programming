750f048319b83a3043d0ebde83aa0ba631214279
Malik <bamjnr99@gmail.com>
2023-03-18 22:03:42 -0700

    a script that takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+""" a script that lists all cities from the database hbtn_0e_4_usa"""
+
+if __name__ == "__main__":
+
+    import MySQLdb
+    from sys import argv
+
+    connection = MySQLdb.connect(
+                                        host="localhost",
+                                        user=argv[1],
+                                        password=argv[2],
+                                        database=argv[3]
+
+                                )
+
+    query = "SELECT cities.name FROM cities\
+            JOIN states ON cities.id = states.id\
+            WHERE BINARY  states.name = '{}'\
+            ORDER BY cities.id".format(argv[4])
+
+    cursor = connection.cursor()
+
+    cursor.execute(query)
+
+    results = cursor.fetchall()
+
+    print(", ".join(result[0] for result in results))
+
+    cursor.close()
+    connection.close()
