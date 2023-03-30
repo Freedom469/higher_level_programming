17fd172943e84eb00e2265201802cd5c133d2ee5
Freedom469 <bamjnr99@gmail.com>
2023-03-30 15:43:44 -0700

    0. cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 #a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -s -o /dev/null  -w "%{size_download}\n" "$1"
+curl -s -w '%{size_download}\n' -o /dev/null "$1"
