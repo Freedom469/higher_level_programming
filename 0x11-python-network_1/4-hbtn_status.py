7c0aebcb19a1c9aef7dcd718ef0f94966c45cc0c
Freedom469 <bamjnr99@gmail.com>
2023-03-31 19:08:44 -0700

    What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""
-Python script that fetches an URL with requests package
-"""
+"""get request with requests package"""
+
 import requests
 
-
 if __name__ == "__main__":
-    r = requests.get('https://intranet.hbtn.io/status')
-    t = r.text
-    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
+    response = requests.get("https://alx-intranet.hbtn.io/status").text
+    print("Body response:")
+    print(f'\t- type: {type(response)}')
+    print(f'\t- content: {response}')
