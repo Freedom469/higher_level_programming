bb0e3cc34dca28b65ced8706704ed102110839a9
Freedom469 <bamjnr99@gmail.com>
2023-03-31 19:10:44 -0700

    Response header value #1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,15 +1,10 @@
-#!/usr/bin/python3
-"""
-Python script that sends a request to the URL and
-displays the value of a variable in the response header
-"""
-import requests
-import sys
 
+#!/usr/bin/python3
+"""retrieve a header from a get request"""
+
+import requests
+from sys import argv
 
 if __name__ == "__main__":
-    try:
-        r = requests.get(sys.argv[1])
-        print(r.headers['X-Request-Id'])
-    except:
-        pass
+    response = requests.get(argv[1])
+    print(response.headers.get('X-Request-Id'))
