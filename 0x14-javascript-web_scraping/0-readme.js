f1758091a36c8ac012f3c91bb2935ea846f3230c
freedom469 <bamjnr99@gmail.com>
2023-04-24 21:34:04 -0700

    a script that reads and prints the content of a file.

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const file = process.argv[2];
+
+try {
+  const data = fs.readFileSync(file, 'utf-8');
+  console.log(data);
+} catch (err) {
+  console.log(err);
+}
