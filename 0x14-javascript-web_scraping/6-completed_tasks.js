121d383038fb54a8151765aa0d8f8af4b967d9b9
freedom469 <bamjnr99@gmail.com>
2023-04-26 20:00:13 -0700

    fix

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,32 +2,24 @@
 
 const request = require('request');
 
-const url = 'https://jsonplaceholder.typicode.com/todos';
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
+request(process.argv[2], (err, response, body) => {
+  if (err) {
+    console.error(err);
     return;
   }
 
-  if (response.statusCode !== 200) {
-    console.error('Invalid status code:', response.statusCode);
-    return;
-  }
+  const json = JSON.parse(body);
+  const re = {};
 
-  const todos = JSON.parse(body);
-
-  const completedByUser = {};
-
-  todos.forEach((todo) => {
-    if (todo.completed) {
-      if (!completedByUser[todo.userId]) {
-        completedByUser[todo.userId] = 1;
-      } else {
-        completedByUser[todo.userId]++;
+  for (let i = 0; i < json.length; i++) {
+    if (json[i].completed === true) {
+      if (re[json[i].userId] === undefined) {
+        re[json[i].userId] = 0;
       }
-    }
-  });
 
-  console.log(completedByUser);
+      re[json[i].userId]++;
+    }
+  }
+
+  console.log(re);
 });
